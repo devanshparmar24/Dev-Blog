de2b0cc02fb05109c6bf6a675f9bea4d59e569e9
devanshparmar24 <devanshparmar13618@gmail.com>
2026-02-15 19:27:19 +0530

    copied the project from learning repo for deployment

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Logo from './Logo';
+import LogoutBtn from './Header/LogoutBtn';
+import Container from './container/Container';
+import Button from './Button';
+import Select from './Select';
+import Input from './Input';
+import RTE from './RTE';
+import Signup from './Signup';
+import Login from './Login';
+import PostCard from './PostCard';
+import PostForm from './post-form/PostForm';
+import AuthLayout from './AuthLayout';
+
+export {
+    Header,
+    Footer,
+    Logo,
+    LogoutBtn,
+    Container,
+    Button,
+    Select,
+    Input,
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout
+}
